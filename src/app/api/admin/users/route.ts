70d79e060fcfeb9ddf4caf4d7c0f244322316d19
Admin SMK Ma'arif <admin@smkmaarifgarut.sch.id>
2026-06-10 00:58:55 +0700

    perubahan bcrypt menjadi bcryptjs

--- src/app/api/admin/users/route.ts
+++ src/app/api/admin/users/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 export async function GET() {
   try {
